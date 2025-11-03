6df97874c8cec0e8ff835585de341858fe16e0d1
2500018015-Ryan-Dwi-Nur-Ikhsan <2500018015@webmail.uad.ac.id>
2025-11-03 23:46:04 +0700

    Add files via upload

---
+++ Soal_level_2_opsional.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int detik,jam,menit,hari;
+    int bilangandetik;
+
+    cout << "Masukkan bilangan >1 (satuan detik) : ";
+    cin >> bilangandetik;
+
+    hari = bilangandetik/86400;
+    jam = (bilangandetik %86400) / 3600 ;
+    menit = (bilangandetik % 3600) / 60;
+    detik = (bilangandetik % 60);
+
+    if (bilangandetik<86400) {
+        cout << bilangandetik << " detik " << "= " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+    } else {
+        cout << bilangandetik << " detik " << "= " << hari << " hari " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+    }
+
+    return 0;
+}
