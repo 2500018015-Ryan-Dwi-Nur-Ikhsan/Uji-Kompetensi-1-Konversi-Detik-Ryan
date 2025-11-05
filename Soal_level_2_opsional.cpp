d487b7e938c869739d314d19f9c10fc7f99ea4fe
2500018015-Ryan-Dwi-Nur-Ikhsan <2500018015@webmail.uad.ac.id>
2025-11-05 09:57:15 +0700

    Update Soal_level_2_opsional.cpp

--- Soal_level_2_opsional.cpp
+++ Soal_level_2_opsional.cpp
@@ -2,21 +2,26 @@
 using namespace std;
 
 int main() {
-    int detik,jam,menit,hari;
+    int detik, jam, menit, hari;
     int bilangandetik;
-
+    
     cout << "Masukkan bilangan >1 (satuan detik) : ";
     cin >> bilangandetik;
 
-    hari = bilangandetik/86400;
-    jam = (bilangandetik %86400) / 3600 ;
-    menit = (bilangandetik % 3600) / 60;
-    detik = (bilangandetik % 60);
+    if (bilangandetik < 1) {
+        cout << "Bilangan detik anda tidak valid karena kurang dari 1.";
+    } 
+    else {
+        hari = bilangandetik / 86400;
+        jam = (bilangandetik % 86400) / 3600;
+        menit = (bilangandetik % 3600) / 60;
+        detik = (bilangandetik % 60);
 
-    if (bilangandetik<86400) {
-        cout << bilangandetik << " detik " << "= " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
-    } else {
-        cout << bilangandetik << " detik " << "= " << hari << " hari, " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+        if (bilangandetik < 86400) {
+            cout << bilangandetik << " detik = " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+        } else {
+            cout << bilangandetik << " detik = " << hari << " Hari, " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+        }
     }
 
     return 0;
