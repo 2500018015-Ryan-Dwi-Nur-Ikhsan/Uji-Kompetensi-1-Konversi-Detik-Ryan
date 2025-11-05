1ba57a960ac30cc166cbd9012ab3e35ccacee46c
2500018015-Ryan-Dwi-Nur-Ikhsan <2500018015@webmail.uad.ac.id>
2025-11-05 10:13:22 +0700

    Update Soal_level_1_wajib.cpp

--- Soal_level_1_wajib.cpp
+++ Soal_level_1_wajib.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main() {
     int detik,jam,menit;
     int bilangandetik;
-
+    
     cout << "Masukkan bilangan >1 (satuan detik) : ";
     cin >> bilangandetik;
 
-    jam = bilangandetik /3600;
-    menit = (bilangandetik % 3600) / 60;
-    detik = (bilangandetik % 60);
+    if (bilangandetik<1) {
+        cout << "Bilangan detik anda tidak valid karena kurang dari 1 atau bukan angka";
+    } else {
+        jam = bilangandetik /3600;
+        menit = (bilangandetik % 3600) / 60;
+        detik = (bilangandetik % 60);
 
-    cout << bilangandetik << " detik " << "= " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
-    
+        cout << bilangandetik << " detik " << " = " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+    }
 
     return 0;
 }
